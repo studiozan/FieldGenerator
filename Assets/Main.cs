34be9d3eacf2a35808043e5779fc05cdce793676
zan-hanawa <r-hanawa@studio-zan.com>
2020-07-16 14:17:05 +0900

    生成を確認するためのスクリプト追加

---
+++ Assets/Main.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FieldGenerator
+{
+	public class Main : MonoBehaviour
+	{
+		void Awake()
+		{
+			generator = GetComponent<TownGenerator>();
+			generator.Initialize();
+		}
+
+		void Start()
+		{
+			StartCoroutine(generator.GenerateTown());
+		}
+
+		void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Space) != false)
+			{
+				StartCoroutine(generator.GenerateTown());
+			}
+		}
+
+		TownGenerator generator;
+	}
+}
