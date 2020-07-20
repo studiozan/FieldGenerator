34af0a6f37a62f2a9eb4f4f22d2ba408d6bf0c48
zan-tamaki <d-tamaki@studio-zan.com>
2020-07-20 14:41:55 +0900

    すごろく用の座標を生成する時に座標を間引く処理を追加

--- Runtime/Scripts/PointConnection.cs
+++ Runtime/Scripts/PointConnection.cs
@@ -27,35 +27,29 @@ namespace FieldGenerator
 		/**
 		 * フィールド座標の接続処理をする
 		 * 川と全ての道路とすごろく用道路のリストを作成する。作成するリストは今後増減する可能性がある。
-		 * @param field_list	接続を行うフィールドポイントのリスト
-		 * @param interval		接続を行う座標の間隔
+		 * @param fieldList		接続を行うフィールドポイントのリスト
+		 * @param interval		接続を行う道路の座標の間隔
+		 * @param riverInterval	接続を行う川の座標の間隔
+		 * @param sugorokuMergeMulti	すごろく用の接続座標を間引く範囲の倍率
 		 */
-		public void FieldConnectCreate( List<FieldPoint> field_list, float interval, float river_interval = 10f)
+		public void FieldConnectCreate( List<FieldPoint> fieldList, float interval, float riverInterval = 10f, float sugorokuMergeMulti = 1.75f)
 		{
-			/*! フィールドの座標情報のリストを設定 */
-			fieldPointList = field_list;
+			/* フィールドの座標情報のリストを設定 */
+			fieldPointList = fieldList;
 
 			SetFieldPoint();
-			var list = new List<int>();
 
-			/*! 川を繋げる */
-			list.Add( (int)PointType.kRiver);
-			SetConnection( riverConnectPointList, river_interval, list, true);
-			/*! 道路を全て繋げる */
-			list.Clear();
-			list.Add((int)PointType.kRoadAlongRiver);
-			list.Add((int)PointType.kGridRoad);
-			list.Add((int)PointType.kDistrictRoad);
-			list.Add((int)PointType.kIntersectionOfGridRoadAndRoadAlongRiver);
-			list.Add((int)PointType.kIntersectionOfGridRoadAndDistrictRoad);
-			list.Add((int)PointType.kIntersectionOfRoadAlongRiverAndDistrictRoad);
-			SetConnection( roadConnectPointList, interval, list);
-			/*! すごろくで使う道路を繋げる */
-			list.Clear();
-			list.Add((int)PointType.kGridRoad);
-			list.Add((int)PointType.kIntersectionOfGridRoadAndRoadAlongRiver);
-			list.Add((int)PointType.kIntersectionOfGridRoadAndDistrictRoad);
-			SetConnection( sugorokuConnectPointList, interval, list);
+			/* 川を繋げる */
+			SetConnection( riverConnectPointList, riverInterval, true);
+			/* 道路を全て繋げる */
+			SetConnection( roadConnectPointList, interval);
+			/* すごろくで使う道路を繋げる */
+			float power = sugorokuMergeMulti;
+			if( power < 1f)
+			{
+				power = 1f;
+			}
+			SetConnection( sugorokuConnectPointList, interval * ( power * 2f - 1f), false, interval * power);
 		}
 
 		/**
@@ -64,9 +58,8 @@ namespace FieldGenerator
 		void SetFieldPoint()
 		{
 			int i0, i1;
-			FieldPoint tmp_field;
-			FieldConnectPoint tmp_point;
-			List<FieldConnectPoint> tmp_list;
+			FieldConnectPoint createPoint;
+			List<FieldConnectPoint> addList;
 			bool flg;
 
 			riverConnectPointList.Clear();
@@ -75,50 +68,49 @@ namespace FieldGenerator
 			
 			for( i0 = 0; i0 < fieldPointList.Count; ++i0)
 			{
-				tmp_field = fieldPointList[ i0];
 				for( i1 = 0; i1 < 3; ++i1)
 				{
 					flg = false;
 					switch( i1)
 					{
 					case 0:
-						tmp_list = riverConnectPointList;
-						if( tmp_field.Type == PointType.kRiver)
+						addList = riverConnectPointList;
+						if( fieldPointList[ i0].Type == PointType.kRiver)
 						{
 							flg = true;
 						}
 						break;
 					case 1:
-						tmp_list = roadConnectPointList;
-						if( tmp_field.Type == PointType.kRoadAlongRiver ||
-							tmp_field.Type == PointType.kGridRoad ||
-							tmp_field.Type == PointType.kDistrictRoad ||
-							tmp_field.Type == PointType.kIntersectionOfGridRoadAndRoadAlongRiver ||
-							tmp_field.Type == PointType.kIntersectionOfGridRoadAndDistrictRoad ||
-							tmp_field.Type == PointType.kIntersectionOfRoadAlongRiverAndDistrictRoad)
+						addList = roadConnectPointList;
+						if( fieldPointList[ i0].Type == PointType.kRoadAlongRiver ||
+							fieldPointList[ i0].Type == PointType.kGridRoad ||
+							fieldPointList[ i0].Type == PointType.kDistrictRoad ||
+							fieldPointList[ i0].Type == PointType.kIntersectionOfGridRoadAndRoadAlongRiver ||
+							fieldPointList[ i0].Type == PointType.kIntersectionOfGridRoadAndDistrictRoad ||
+							fieldPointList[ i0].Type == PointType.kIntersectionOfRoadAlongRiverAndDistrictRoad)
 						{
 							flg = true;
 						}
 						break;
 					case 2:
-						tmp_list = sugorokuConnectPointList;
-						if( tmp_field.Type == PointType.kGridRoad ||
-							tmp_field.Type == PointType.kIntersectionOfGridRoadAndRoadAlongRiver ||
-							tmp_field.Type == PointType.kIntersectionOfGridRoadAndDistrictRoad)
+						addList = sugorokuConnectPointList;
+						if( fieldPointList[ i0].Type == PointType.kGridRoad ||
+							fieldPointList[ i0].Type == PointType.kIntersectionOfGridRoadAndRoadAlongRiver ||
+							fieldPointList[ i0].Type == PointType.kIntersectionOfGridRoadAndDistrictRoad)
 						{
 							flg = true;
 						}
 						break;
 					default:
-						tmp_list = riverConnectPointList;
+						addList = riverConnectPointList;
 						flg = false;
 						break;
 					}
 					if( flg != false)
 					{
-						tmp_point = new FieldConnectPoint();
-						tmp_point.Initialize( tmp_field.Position, tmp_field.Type);
-						tmp_list.Add( tmp_point);
+						createPoint = new FieldConnectPoint();
+						createPoint.Initialize( fieldPointList[ i0].Position, fieldPointList[ i0].Type);
+						addList.Add( createPoint);
 					}
 				}
 			}
@@ -126,126 +118,102 @@ namespace FieldGenerator
 
 		/**
 		 * ポイントのリストを元に接続の処理を行う
-		 * @param point_list	ポイントクラスのリスト
+		 * @param pointList	ポイントクラスのリスト
 		 * @param interval		繋がる座標感の幅
-		 * @param specific_list	接続を行う属性のテーブル
-		 * @param in_order		順番通りに繋げるフラグ。川のように座標が順番に生成されるような時にtrueにする
+		 * @param inOrder		順番通りに繋げるフラグ。川のように座標が順番に生成されるような時にtrueにする
+		 * @param mergeSize		頂点を融合させる時の判定サイズ
 		 * @param random		接続する確率
-		 * @param max_num		接続する最大数。-1の場合は判定しない
+		 * @param maxNum		接続する最大数。-1の場合は判定しない
 		 */
-		public void SetConnection( List<FieldConnectPoint> point_list, float interval, List<int> specific_list, bool in_order = false,
-			float random = 1f, int max_num = -1)
+		public void SetConnection( List<FieldConnectPoint> pointList, float interval, bool inOrder = false, float mergeSize = 0f,
+			float random = 1f, int maxNum = -1)
 		{
-			int i0, i1, i2, tmp_i, random_count, count;
-			float itv, length, theta = 0.707f, tmp_f, rand;
-			bool flg, order_flg;
+			int i0, i1, i2, index, randomCount, count;
+			float itv, length, checkTheta = 0.707f, theta, rand;
+			bool flg, orderFlag;
 			Vector3 sub;
-			FieldConnectPoint tmp_point;
+			FieldConnectPoint currentPoint;
 			var direction = new Vector3[ 4];
 			var min = new float[ 4];
 			var no = new int[ 4];
 			itv = interval * 1.5f;
 			itv = itv * itv;
-			random_count = 0;
+			randomCount = 0;
 
 			direction[ 0] = Vector3.forward;
 			direction[ 1] = Vector3.back;
 			direction[ 2] = Vector3.right;
 			direction[ 3] = Vector3.left;
 
-			/* ランダムを使う場合は双方向で繋がるようにしないとダメかもしれない */
-
-			for( i0 = 0; i0 < point_list.Count; ++i0)
+			if( mergeSize > 0f)
 			{
-				tmp_point = point_list[ i0];
-				flg = false;
-				for( i1 = 0; i1 < specific_list.Count; ++i1)
+				/* 頂点を間引く処理 */
+				FieldPointMerge( pointList, mergeSize);
+			}
+
+			for( i0 = 0; i0 < pointList.Count; ++i0)
+			{
+				currentPoint = pointList[ i0];
+				if( maxNum >= 0 && maxNum <= randomCount)
 				{
-					if( (int)tmp_point.Type == specific_list[ i1])
-					{
-						flg = true;
-						break;
-					}
-				}
-				if( flg == false)
-				{
-					/*! 使用する属性じゃないので判定しない */
-					continue;
-				}
-				if( max_num >= 0 && max_num <= random_count)
-				{
-					/*! ランダムで作る最大数に達しているので処理を終わる */
+					/* ランダムで作る最大数に達しているので処理を終わる */
 					return;
 				}
 				rand = (float)randomSystem.NextDouble();
 				if( rand > random)
 				{
-					/*! ランダムに判定しない */
+					/* ランダムに判定しない */
 					continue;
 				}
 				// TODO: 4方向だけに繋げるための初期化だが、すでに接続してる物がある場合を考慮していなかったので、繋げているものがある場合はその値で初期化するように変更したい
 				min[ 0] = itv;	min[ 1] = itv;	min[ 2] = itv;	min[ 3] = itv;
 				no[ 0] = -1;	no[ 1] = -1;	no[ 2] = -1;	no[ 3] = -1;
-				order_flg = false;
-				count = tmp_point.ConnectionList.Count;
-				for( i1 = 0; i1 < point_list.Count; ++i1)
+				orderFlag = false;
+				count = currentPoint.ConnectionList.Count;
+				for( i1 = 0; i1 < pointList.Count; ++i1)
 				{
-					if( in_order != false && count > 0)
+					if( inOrder != false && count > 0)
 					{
-						/*! 順番通りの場合は次の座標と判断する */
-						if( order_flg == false)
+						/* 順番通りの場合は次の座標と判断する */
+						if( orderFlag == false)
 						{
 							if( i0 < i1)
 							{
 								break;
 							}
 							i1 = i0 + 1;
-							if( i1 >= point_list.Count)
+							if( i1 >= pointList.Count)
 							{
 								break;
 							}
-							order_flg = true;
+							orderFlag = true;
 						}
 					}
 					if( i0 == i1)
 					{
-						/*! 同じものは判定しない */
+						/* 同じものは判定しない */
 						continue;
 					}
-					flg = false;
-					for( i2 = 0; i2 < specific_list.Count; ++i2)
-					{
-						if( (int)point_list[ i1].Type == specific_list[ i2])
-						{
-							flg = true;
-							break;
-						}
-					}
-					if( flg == false)
-					{
-						/*! 使用する属性じゃないので判定しない */
-						continue;
-					}
-					sub = point_list[ i1].Position - tmp_point.Position;
+					sub = pointList[ i1].Position - currentPoint.Position;
 					length = sub.x * sub.x + sub.z * sub.z;
 					if( length > itv)
 					{
-						/*! 距離が離れているものは判定しない */
+						/* 距離が離れているものは判定しない */
 						continue;
 					}
 					sub = sub.normalized;
 					flg = false;
 					for( i2 = 0; i2 < direction.Length; ++i2)
 					{
-						tmp_f = sub.x * direction[ i2].x + sub.z * direction[ i2].z;
-						if( tmp_f <= theta)
+						theta = sub.x * direction[ i2].x + sub.z * direction[ i2].z;
+						if( theta <= checkTheta)
 						{
-							/*! 角度の条件を満たしていない */
+							/* 角度の条件を満たしていない */
 							continue;
 						}
 						if( min[ i2] <= length)
 						{
-							/*! すでに設定しているものより遠い */
+							/* すでに設定しているものより遠い */
 							continue;
 						}
 						min[ i2] = length;
@@ -256,11 +224,11 @@ namespace FieldGenerator
 					{
 						++count;
 					}
-					if( in_order != false)
+					if( inOrder != false)
 					{
-						if( order_flg != false)
+						if( orderFlag != false)
 						{
-							i1 = point_list.Count;
+							i1 = pointList.Count;
 						}
 					}
 				}
@@ -268,14 +236,47 @@ namespace FieldGenerator
 				{
 					if( no[ i2] < 0)
 					{
-						/*! この方向に繋げる座標が無かった */
+						/* この方向に繋げる座標が無かった */
 						continue;
 					}
-					tmp_i = no[ i2];
-					tmp_point.SetConnection( point_list[ tmp_i]);
-					point_list[ tmp_i].SetConnection( tmp_point);
+					index = no[ i2];
+					currentPoint.SetConnection( pointList[ index]);
+					pointList[ index].SetConnection( currentPoint);
 				}
-				++random_count;
+				++randomCount;
+			}
+		}
+
+		/**
+		 * 接続ポイントを間引く処理
+		 */
+		void FieldPointMerge( List<FieldConnectPoint> pointList, float mergeSize)
+		{
+			int i0, i1;
+			float powerSize = mergeSize * mergeSize, length;
+			Vector3 sub;
+			var indexList = new List<int>();
+
+			for( i0 = 0; i0 < pointList.Count - 1; ++i0)
+			{
+				indexList.Clear();
+				for( i1 = i0 + 1; i1 < pointList.Count; ++i1)
+				{
+					if( i0 == i1)
+					{
+						continue;
+					}
+					sub = pointList[ i0].Position - pointList[ i1].Position;
+					length = sub.x * sub.x + sub.z * sub.z;
+					if( powerSize > length)
+					{
+						indexList.Add(i1);
+					}
+				}
+				for( i1 = indexList.Count - 1; i1 >= 0; --i1)
+				{
+					pointList.RemoveAt( indexList[ i1]);
+				}
 			}
 		}
 
@@ -305,11 +306,11 @@ namespace FieldGenerator
 
 		System.Random randomSystem;
 		List<FieldPoint> fieldPointList;
-		/*! 川の接続リスト */
+		/* 川の接続リスト */
 		List<FieldConnectPoint> riverConnectPointList;
-		/*! 全ての道路の接続リスト */
+		/* 全ての道路の接続リスト */
 		List<FieldConnectPoint> roadConnectPointList;
-		/*! すごろくで使用する接続リスト */
+		/* すごろくで使用する接続リスト */
 		List<FieldConnectPoint> sugorokuConnectPointList;
 	}
 }
