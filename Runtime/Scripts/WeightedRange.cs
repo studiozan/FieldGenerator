768411f0b191dc448d0afc1b0ca30335c289b9db
zan-hanawa <r-hanawa@studio-zan.com>
2020-09-03 18:45:37 +0900

    重み付きの範囲を表すクラス追加

---
+++ Runtime/Scripts/WeightedRange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FieldGenerator
+{
+	[System.Serializable]
+	public class WeightedRange
+	{
+		[SerializeField]
+		public float min = default;
+		[SerializeField]
+		public float max = default;
+		[SerializeField]
+		public float weight = default;
+	}
+}
