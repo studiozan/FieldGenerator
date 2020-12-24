4746b999242d7995540f1cce7ce450ad5ab39ca7
zan-hanawa <r-hanawa@studio-zan.com>
2020-12-24 21:12:53 +0900

    SurroundedAreaクラスに中心を取得する関数を追加

--- Runtime/Scripts/SurroundedArea.cs
+++ Runtime/Scripts/SurroundedArea.cs
@@ -6,6 +6,18 @@ namespace FieldGenerator
 {
 	public class SurroundedArea
 	{
+		public Vector3 GetCenter()
+		{
+			Vector3 center = Vector3.zero;
+			for (int i0 = 0; i0 < AreaPoints.Count; ++i0)
+			{
+				center += AreaPoints[i0];
+			}
+			center /= AreaPoints.Count;
+
+			return center;
+		}
+
 		public List<Vector3> AreaPoints { get; set; }
 	}
 }
